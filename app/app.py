fee373945ce64980e3bbdf738388d9befa4cfa25
root <root@DESKTOP-1TL7IR5.localdomain>
2026-03-27 16:11:40 +0300

    Added secete keys

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route("/")
 def home():
-    return "Stripe Gatway payment!"
+    return "Stripe Gateway Payment!"
 
 if __name__ == "__main__":
     app.run(debug=True)
